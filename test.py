d1592ea56a91446b8458d79ab868787f298c78f6
mikkel lindtner <mkl@mikiaholding.dk>
2023-11-06 15:26:29 +0100

    first actual commit

---
+++ test.py
@@ -0,0 +1,126 @@
+import math
+import timeit
+
+import librosa
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import pandas as pd
+import resampy
+from IPython.display import Audio
+
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+
+DEFAULT_OFFSET = 201
+
+print("Hej")
+
+def _get_log_freq(sample_rate, max_sweep_rate, offset):
+    """Get freqs evenly spaced out in log-scale, between [0, max_sweep_rate // 2]
+
+    offset is used to avoid negative infinity `log(offset + x)`.
+
+    """
+    start, stop = math.log(offset), math.log(offset + max_sweep_rate // 2)
+    return torch.exp(torch.linspace(start, stop, sample_rate, dtype=torch.double)) - offset
+
+
+def _get_inverse_log_freq(freq, sample_rate, offset):
+    """Find the time where the given frequency is given by _get_log_freq"""
+    half = sample_rate // 2
+    return sample_rate * (math.log(1 + freq / offset) / math.log(1 + half / offset))
+
+
+def _get_freq_ticks(sample_rate, offset, f_max):
+    # Given the original sample rate used for generating the sweep,
+    # find the x-axis value where the log-scale major frequency values fall in
+    times, freq = [], []
+    for exp in range(2, 5):
+        for v in range(1, 10):
+            f = v * 10**exp
+            if f < sample_rate // 2:
+                t = _get_inverse_log_freq(f, sample_rate, offset) / sample_rate
+                times.append(t)
+                freq.append(f)
+    t_max = _get_inverse_log_freq(f_max, sample_rate, offset) / sample_rate
+    times.append(t_max)
+    freq.append(f_max)
+    return times, freq
+
+
+def get_sine_sweep(sample_rate, offset=DEFAULT_OFFSET):
+    max_sweep_rate = sample_rate
+    freq = _get_log_freq(sample_rate, max_sweep_rate, offset)
+    delta = 2 * math.pi * freq / sample_rate
+    cummulative = torch.cumsum(delta, dim=0)
+    signal = torch.sin(cummulative).unsqueeze(dim=0)
+    return signal
+
+
+def plot_sweep(
+    waveform,
+    sample_rate,
+    title,
+    max_sweep_rate=48000,
+    offset=DEFAULT_OFFSET,
+):
+    x_ticks = [100, 500, 1000, 5000, 10000, 20000, max_sweep_rate // 2]
+    y_ticks = [1000, 5000, 10000, 20000, sample_rate // 2]
+
+    time, freq = _get_freq_ticks(max_sweep_rate, offset, sample_rate // 2)
+    freq_x = [f if f in x_ticks and f <= max_sweep_rate // 2 else None for f in freq]
+    freq_y = [f for f in freq if f in y_ticks and 1000 <= f <= sample_rate // 2]
+
+    figure, axis = plt.subplots(1, 1)
+    _, _, _, cax = axis.specgram(waveform[0].numpy(), Fs=sample_rate)
+    plt.xticks(time, freq_x)
+    plt.yticks(freq_y, freq_y)
+    axis.set_xlabel("Original Signal Frequency (Hz, log scale)")
+    axis.set_ylabel("Waveform Frequency (Hz)")
+    axis.xaxis.grid(True, alpha=0.67)
+    axis.yaxis.grid(True, alpha=0.67)
+    #figure.suptitle(f"{title} (sample rate: {sample_rate} Hz)")
+    plt.colorbar(cax)
+import dac
+from audiotools import AudioSignal
+import torch
+import torchaudio
+import soundfile as sf
+import torchaudio.functional as F
+import torchaudio.transforms as T
+sample_rate = 48000
+waveform = get_sine_sweep(sample_rate)
+# Download a model
+model_path = dac.utils.download(model_type="44khz")
+model = dac.DAC.load(model_path)
+
+model.to('cuda')
+
+# Load audio signal file
+waveform, sample_rate = torchaudio.load("Input.wav", normalize=True, backend = 'soundfile')
+transform = T.Resample(48000, 44100)
+signal = transform(waveform)
+signal = AudioSignal(signal,44100)
+# Encode audio signal as one long file
+# (may run out of GPU memory on long files)
+signal.to(model.device)
+x = model.preprocess(signal.audio_data, signal.sample_rate)
+z, codes, latents, _, _ = model.encode(x)
+
+# Decode audio xsignal
+y = model.decode(z)
+
+# Alternatively, use the `compress` and `decompress` functions
+# to compress long files.
+
+signal = signal.cpu()
+x = model.compress(signal)
+
+# Save and load to and from disk
+x.save("compressed.dac")
+x = dac.DACFile.load("compressed.dac")
+
+# Decompress it back to an AudioSignal
+y = model.decompress(x)
+# Write to file
+y.write('output.wav')
