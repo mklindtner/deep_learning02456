543bfe6538a36203958950bbb4fd994371705276
MakkerW <119938215+MakkerW@users.noreply.github.com>
2023-12-02 12:11:11 +0100

    Hurtig træning

--- training.py
+++ training.py
@@ -3,16 +3,23 @@ import os
 from dataset import data_loader_speech_train, data_loader_noise
 import torchaudio.functional as F
 import torch
+import numpy as np
 from audiotools import AudioSignal
 from torchmetrics.audio import SignalNoiseRatio
 # from torchmetrics.audio import SignalNoiseRatio
-snr = SignalNoiseRatio()
+snr = SignalNoiseRatio().to('cuda')
 model_path = dac.utils.download(model_type="44khz")
 model = dac.DAC.load(model_path)
-# model.to('cuda')
+model.to('cuda')
 optimizer = torch.optim.AdamW(model.parameters(), lr=0.00001)
-epochs = 1
-
+epochs = 10
+idy, noise_test = (next(enumerate(data_loader_noise)))
+idy, speech_test = (next(enumerate(data_loader_speech_train)))
+speech_test, sample_rate = speech_test
+noise_test, _ = noise_test
+signal_test = F.add_noise(speech_test, noise_test, snr=torch.tensor([[2.0]]))
+signal_test = AudioSignal(signal_test, sample_rate=sample_rate)
+signal_test.to('cuda')
 #Epochs
 for i in range(epochs):
     for idx, speech in enumerate(data_loader_speech_train):
@@ -24,7 +31,7 @@ for i in range(epochs):
         # speech_element = speech[torch.arange(speech.size(0)), 0]
         # noise_element = noise[torch.arange(speech.size(0)), 0]
         
-        signal = F.add_noise(speech,noise, snr=torch.tensor([[1.1]]))
+        signal = F.add_noise(speech,noise, snr=torch.tensor([[2.0]]))
 
         # Combine noise and speech for the training set element
         # signal = F.add_noise(speech_element, noise_element, snr=torch.tensor([[1.5]]))     
@@ -32,7 +39,8 @@ for i in range(epochs):
         signal = AudioSignal(signal, sample_rate=sample_rate)
         #test signal.sample_rate vs sample_rate
 
-        # signal.to('cuda')
+        signal.to('cuda')
+        speech = speech.to('cuda')
         #Use Decript Model To encode
         x = model.preprocess(signal.audio_data, signal.sample_rate)
         #Generate a batch size of (1,y,z)
@@ -44,35 +52,26 @@ for i in range(epochs):
         
         #label - Doesn't work right now
         y = model.decode(z)        
-        print(y)
         #Loss SNR heremsd
         y = y[:, :, :speech.size(2)]
-        loss = 1/snr(y,speech)
-        print("loss:", loss)
+        loss = torch.nn.functional.mse_loss(y, speech)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        metrics = snr(y, speech)
+        '''y_audio_signal = AudioSignal(y.cpu().detach().numpy(), sample_rate=sample_rate)
+        y_audio_signal.write(f"output{i}.wav")'''
         print(f'loss: {loss}')
-        if loss < 0.3:
-            y_audio_signal = AudioSignal(y.cpu().detach().numpy(), sample_rate=sample_rate)
-            y_audio_signal.write("output.wav")
-            speech_audio_signal = AudioSignal(signal.cpu().detach().numpy(), sample_rate=sample_rate)
-            speech_audio_signal.write("input.wav")
-
-        if idx % 50 == 0:
-            # print("test")
-            # print(signal.data)
-            # print(y)
-            # print(f'x:{x} \n {x.shape} \n y:{y} \n {y.shape} \n --- \n ')
-            
-            # print(f'{signal.shape}')
-            # print(noise_item)
-            # print(f'noise_batch_len: {len(noise_item)} \t speech_batch_len: {len(item)})')            
-            # print(f'item: {speech[torch.arange(speech.size(0)), 0].shape}')
-            # print(noise.shape)
-            # print(speech.shape)            
-            # print(speech)
-            print("----")
-            # print(noise)
-            # print(f'item {signal} \n')
-        #Combine speech and noise
+        print(f'snr: {metrics}')
+    print("Epoch: ", i)
+    x = model.preprocess(signal_test.audio_data, signal_test.sample_rate)
+    z, codes, latents, _, _ = model.encode(x) 
+    y = model.decode(z)
+    y = y[:, :, :speech.size(2)]
+    y_numpy = y.cpu().detach().numpy()
+    y_normalized = np.clip(y_numpy, -1.0, 1.0)  
+    y_audio_signal = AudioSignal(y_normalized, sample_rate=sample_rate)
+    print("OUTPUT")
+    y_audio_signal.write(f"outpuut{i}.wav")
+    speech_audio_signal = AudioSignal(speech_test, sample_rate=sample_rate)
+    speech_audio_signal.write(f"inpuut{i}.wav")
